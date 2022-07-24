da48eb0689cc971fe438c1ce4df9c89d37b257e8
sshhsh <sshhsh@foxmail.com>
2022-07-24 17:18:07 +0800

    scrypt

--- lib/scrypt.dart
+++ lib/scrypt.dart
@@ -1,13 +1,7 @@
-
-
-// blockCopy copies n numbers from src into dst.
 import 'dart:typed_data';
 
 import 'package:binary/binary.dart';
-
-void blockCopy(Uint32List dst, Uint32List src, int n) {
-	dst.setRange(0, n, src);
-}
+import 'package:cryptography/cryptography.dart';
 
 // blockXOR XORs numbers from dst with n numbers from src.
 void blockXOR(Uint32List dst, Uint32List src, int n) {
@@ -18,8 +12,8 @@ void blockXOR(Uint32List dst, Uint32List src, int n) {
 
 extension RotateLeft32 on int {
   int rotateLeft32(int n) {
-    var x = this & 4294967295;
-    return Uint32(x).rotateRightShift(-n).value;
+    var x = this & 0xffffffff;
+    return (x << n | x >> (32 - n)) & 0xffffffff;
   }
 }
 
@@ -31,42 +25,42 @@ void salsaXOR(Uint32List tmp, Uint32List inp, Uint32List out) {
     w[i] = tmp[i] ^ inp[i];
     x[i] = w[i];
   }
-  for (var i = 0; i < 8; i+=2) {
+  for (var i = 0; i < 8; i += 2) {
     x[4] ^= (x[0] + x[12]).rotateLeft32(7);
     x[8] ^= (x[4] + x[0]).rotateLeft32(9);
     x[12] ^= (x[8] + x[4]).rotateLeft32(13);
     x[0] ^= (x[12] + x[8]).rotateLeft32(18);
-    
+
     x[9] ^= (x[5] + x[1]).rotateLeft32(7);
     x[13] ^= (x[9] + x[5]).rotateLeft32(9);
     x[1] ^= (x[13] + x[9]).rotateLeft32(13);
     x[5] ^= (x[1] + x[13]).rotateLeft32(18);
-    
+
     x[14] ^= (x[10] + x[6]).rotateLeft32(7);
     x[2] ^= (x[14] + x[10]).rotateLeft32(9);
     x[6] ^= (x[2] + x[14]).rotateLeft32(13);
     x[10] ^= (x[6] + x[2]).rotateLeft32(18);
-    
+
     x[3] ^= (x[15] + x[11]).rotateLeft32(7);
     x[7] ^= (x[3] + x[15]).rotateLeft32(9);
     x[11] ^= (x[7] + x[3]).rotateLeft32(13);
     x[15] ^= (x[11] + x[7]).rotateLeft32(18);
-    
+
     x[1] ^= (x[0] + x[3]).rotateLeft32(7);
     x[2] ^= (x[1] + x[0]).rotateLeft32(9);
     x[3] ^= (x[2] + x[1]).rotateLeft32(13);
     x[0] ^= (x[3] + x[2]).rotateLeft32(18);
-    
+
     x[6] ^= (x[5] + x[4]).rotateLeft32(7);
     x[7] ^= (x[6] + x[5]).rotateLeft32(9);
     x[4] ^= (x[7] + x[6]).rotateLeft32(13);
     x[5] ^= (x[4] + x[7]).rotateLeft32(18);
-    
+
     x[11] ^= (x[10] + x[9]).rotateLeft32(7);
     x[8] ^= (x[11] + x[10]).rotateLeft32(9);
     x[9] ^= (x[8] + x[11]).rotateLeft32(13);
     x[10] ^= (x[9] + x[8]).rotateLeft32(18);
-    
+
     x[12] ^= (x[15] + x[14]).rotateLeft32(7);
     x[13] ^= (x[12] + x[15]).rotateLeft32(9);
     x[14] ^= (x[13] + x[12]).rotateLeft32(13);
@@ -79,13 +73,94 @@ void salsaXOR(Uint32List tmp, Uint32List inp, Uint32List out) {
   }
 }
 
-void main(List<String> args) {
-  Uint32List tmp = Uint32List.fromList([1,1,4,8,1,1,4,8,1,1,4,8,1,1,4,8,]);
-  Uint32List inp = Uint32List.fromList([5,6,7,9,5,6,7,9,5,6,7,9,5,6,7,9,]);
-  
-  Uint32List out = Uint32List(16);
+void blockMix(Uint32List inp, Uint32List out, int r) {
+  var tmp = inp.sublist((2 * r - 1) * 16, 2 * r * 16);
+  for (var i = 0; i < 2 * r; i += 2) {
+    salsaXOR(tmp, Uint32List.sublistView(inp, i * 16),
+        Uint32List.sublistView(out, i * 8));
+    salsaXOR(tmp, Uint32List.sublistView(inp, (i + 1) * 16),
+        Uint32List.sublistView(out, (i + 2 * r) * 8));
+  }
+}
 
-  salsaXOR(tmp, inp, out);
+int integer(Uint32List b, int r) {
+  var j = (2 * r - 1) * 16;
+  return b[j] | b[j + 1] << 32;
+}
 
-  print(tmp);
+void smix(Uint8List b, int r, int N, Uint32List v, Uint32List x, Uint32List y) {
+  var R = 32 * r;
+  var bView = ByteData.sublistView(b);
+  for (var i = 0, j = 0; i < x.length; i++, j += 4) {
+    x[i] = bView.getUint32(j, Endian.little);
+  }
+  for (var i = 0; i < N; i += 2) {
+    v.setRange(i * R, (i + 1) * R, x);
+    blockMix(x, y, r);
+
+    v.setRange((i + 1) * R, (i + 2) * R, y);
+    blockMix(y, x, r);
+  }
+
+  for (var i = 0, j = 0; i < N; i += 2) {
+    j = integer(x, r) & (N - 1);
+    blockXOR(x, Uint32List.sublistView(v, j * R), R);
+    blockMix(x, y, r);
+
+    j = integer(y, r) & (N - 1);
+    blockXOR(y, Uint32List.sublistView(v, j * R), R);
+    blockMix(y, x, r);
+  }
+
+  for (var i = 0, j = 0; i < x.length; i++, j += 4) {
+    bView.setUint32(j, x[i], Endian.little);
+  }
+}
+
+Future<Uint8List> scrypt(
+    Uint8List password, Uint8List salt, int N, int r, int p, int keyLen) async {
+  if (N <= 1 || N & (N - 1) != 0) {
+    throw Exception("scrypt: N must be > 1 and a power of 2");
+  }
+  if (r * p >= 1 << 30 ||
+      r > (1 << 31) / 128 / p ||
+      r > (1 << 31) / 256 ||
+      N > (1 << 31) / 128 / r) {
+    throw Exception("scrypt: parameters are too large");
+  }
+
+  var x = Uint32List(32 * r);
+  var y = Uint32List(32 * r);
+  var v = Uint32List(32 * N * r);
+
+  // var b = pbkdf2(passphrase, salt, 1, p * 128 * r, "SHA-256");
+  final b = Uint8List.fromList(await (await Pbkdf2(
+    macAlgorithm: Hmac.sha256(),
+    iterations: 1,
+    bits: p * 128 * r * 8,
+  ).deriveKey(
+    secretKey: SecretKey(password),
+    nonce: salt,
+  ))
+      .extractBytes());
+
+  for (var i = 0; i < p; i++) {
+    smix(Uint8List.sublistView(b, i * 128 * r), r, N, v, x, y);
+  }
+
+  return Uint8List.fromList(await (await Pbkdf2(
+    macAlgorithm: Hmac.sha256(),
+    iterations: 1,
+    bits: keyLen * 8,
+  ).deriveKey(
+    secretKey: SecretKey(password),
+    nonce: b,
+  ))
+      .extractBytes());
+}
+
+Future main(List<String> args) async {
+  var a = DateTime.now().microsecondsSinceEpoch;
+  print(await scrypt(Uint8List.fromList([0,0,0,0]), Uint8List.fromList([0,0,0,0]), 32768/*= n*/, 8/*= r*/, 2/*= p*/, 64));
+  print(DateTime.now().microsecondsSinceEpoch - a);
 }
